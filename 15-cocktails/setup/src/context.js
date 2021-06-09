bdf58dd694dd09345b4932ef3ff330d990e618fa
Steffen Leppin <leppinsteffen@gmail.com>
2021-06-09 17:28:46 +0200

    done

--- 15-cocktails/setup/src/context.js
+++ 15-cocktails/setup/src/context.js
@@ -1,11 +1,54 @@
 import React, { useState, useContext, useEffect } from 'react'
 import { useCallback } from 'react'
+import axios from 'axios';
 
 const url = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s='
 const AppContext = React.createContext()
 
 const AppProvider = ({ children }) => {
-  return <AppContext.Provider value='hello'>{children}</AppContext.Provider>
+  const [loading, setLoading] = useState(true);
+  const [searchTerm, setSearchTerm] = useState('a');
+  const [cocktails, setCocktails] = useState([]);
+
+  const fetchDrinks = useCallback(async () => {
+    setLoading(true)
+    try {
+      const response = await axios.get(`${url}${searchTerm}`);
+      const { drinks } = response.data;
+      if (drinks) {
+        const newCocktails = drinks.map(item => {
+          const { idDrink, strDrink, strDrinkThumb, strAlcoholic, strGlass } = item;
+          return {
+            id: idDrink,
+            name: strDrink,
+            image: strDrinkThumb,
+            info: strAlcoholic,
+            glass: strGlass
+          }
+        })
+        setCocktails(newCocktails)
+      } else {
+        setCocktails([])
+      }
+      setLoading(false)
+    } catch (err) {
+      console.log(err);
+      setLoading(false)
+    }
+  }, [searchTerm])
+
+  useEffect(() => {
+    fetchDrinks()
+  }, [searchTerm, fetchDrinks])
+  return (
+    <AppContext.Provider
+      value={ {
+        loading,
+        searchTerm,
+        cocktails,
+        setSearchTerm,
+      } }>{ children }</AppContext.Provider>
+  )
 }
 // make sure use
 export const useGlobalContext = () => {
--- 15-cocktails/setup/src/pages/About.js
+++ 15-cocktails/setup/src/pages/About.js
@@ -2,9 +2,10 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
+    <section className="about-section">
+      <h1 className="section-title">about us</h1>
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nesciunt minus odio veniam praesentium sunt non rerum amet provident doloremque reprehenderit, iure nihil optio blanditiis sapiente neque cum porro accusamus ipsum similique rem repudiandae excepturi qui, natus quibusdam! Laboriosam, asperiores eos!</p>
+    </section>
   )
 }
 
